0a596a6e41e63a836f4dc5688d0955a39237059b
FedyBenMahfoudh <fedi.benmahfoudh@isimm.u-monastir.tn>
2026-01-28 19:31:40 +0100

    initial commit

---
+++ src/main/java/org/example/arabicsearchengine/MainController.java
@@ -0,0 +1,14 @@
+package org.example.arabicsearchengine;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
